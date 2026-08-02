65d19081b6910cb9345c558001c234f695cb2561
aryeeteypetrine1-sketch <aryeeteypetrine1@gmail.com>
2026-08-02 22:13:22 +0000

    Add prime number checking functionality

--- assignment_01_prime_checker.cpp
+++ assignment_01_prime_checker.cpp
@@ -37,3 +37,37 @@
 #include <iostream>
 using namespace std;
 
+
+bool isPrime(int number) {
+    if (number < 2) {
+        return false;
+    }
+
+    for (int i = 2; i * i <= number; i++) {
+        if (number % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int main() {
+    int number;
+
+    std::cout << "Enter a number: ";
+    if (!(std::cin >> number)) {
+        std::cout << "Invalid input. Please enter an integer." << std::endl;
+        return 1;
+    }
+
+    if (isPrime(number)) {
+        std::cout << number << " is a prime number." << std::endl;
+    } else {
+        std::cout << number << " is NOT a prime number." << std::endl;
+    }
+
+    return 0;
+}
+
+
